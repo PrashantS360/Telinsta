478dbf37a4977f6579987c78b030e35608e30615
Prashant Singh <prashantsingh5aug@gmail.com>
2022-07-02 21:50:17 +0530

    Fixing socket issue

--- pages/api/chat/socket.js
+++ pages/api/chat/socket.js
@@ -6,7 +6,7 @@ const SocketHandler = (req, res) => {
         console.log('Socket is already running')
     } else {
         console.log('Socket is initializing')
-        const io = new Server(res.socket.server)
+        const io = new Server(res.socket.server,{ cors: { origin: '*' } })
         res.socket.server.io = io
 
         io.on('connection', socket => {
